488f4ae9cc065d3239e69dce3e79b371a4d654e1
alejandropl07 <aprietoleon97@gmail.com>
2022-04-29 01:56:47 -0500

    Modificada estructura PDF

--- src/components/PDF.js
+++ src/components/PDF.js
@@ -1,14 +1,29 @@
 import React from "react";
 import { Document, Page, Text, View }   from    "@react-pdf/renderer";
 
-const PDF = () => {
+const PDF = ({solicitud}) => {
   return (
     <Document>
         <Page   size="A4">
             <View>
-                <Text>asdasdsadasdsadsa</Text>
-                <Text>asddsadsadaewqewewwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww</Text>
-                <Text>asddsadsadaewqewewwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww</Text>
+                <Text style={{
+                  display:  "flex",
+                  flexDirection:  "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}>Anexo 1 - Modelo de Solicitud de Pedidos</Text>
+                <Text>División</Text>
+                <Text>Sucursal</Text>
+                <Text>Se requiere la aprovación de los recursos en las cantidades y códigos que se anexan 
+                  a este modelo y en las condiciones que a continuación se especifican.
+                </Text>
+                <Text>Pedido No.</Text>
+                <Text>Valor y moneda</Text>
+                <Text>Proveedor</Text>
+                <Text>Contrato(Nr)</Text>
+
+                <Text>Cliente</Text>
+                <Text>Referencia</Text>
             </View>
         </Page>
     </Document>
